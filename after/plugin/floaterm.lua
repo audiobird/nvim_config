15c7ebb15a24d51f34b97b2684fc9e7c2f10c7c6
audiobird <cantrellhepner@gmail.com>
2024-01-18 16:13:43 -0800

    change floatterm map

--- after/plugin/floaterm.lua
+++ after/plugin/floaterm.lua
@@ -1,2 +1,3 @@
-vim.keymap.set("n", "<C-t>", "<cmd>FloatermToggle<CR>")
-vim.api.nvim_set_keymap('t', '<C-t>', '<C-\\><C-n>:FloatermToggle<CR>', {noremap = true, silent = true})
+vim.keymap.set("n", "<F12>", "<cmd>FloatermToggle<CR>")
+vim.api.nvim_set_keymap('t', '<F12>', '<C-\\><C-n>:FloatermToggle<CR>', {noremap = true, silent = true})
+
