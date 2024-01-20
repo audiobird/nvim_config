f793eb3421f4d814766b4727143b7fadc1fddf5b
audiobird <cantrellhepner@gmail.com>
2024-01-20 10:54:30 -0800

    add cmp.lua

---
+++ after/plugin/cmp.lua
@@ -0,0 +1,43 @@
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+--test
+cmp.setup({
+    completion = {
+        keyword_length = 2,
+    },
+    snippet = {
+        -- REQUIRED - you must specify a snippet engine
+        expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        end,
+    },
+    window = {
+        -- completion = cmp.config.window.bordered(),
+        -- documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+        -- `Enter` key to confirm completion
+        ['<CR>'] = cmp.mapping.confirm({select = true}),
+
+        -- Ctrl+Space to trigger completion menu
+        ['<C-Space>'] = cmp.mapping.complete(),
+
+        -- Navigate between snippet placeholder
+        ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+        ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+
+        -- Scroll up and down in the completion documentation
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+        ['<C-j>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+        ['<C-k>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+    }),
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp_signature_help' },
+        { name = 'nvim_lsp' },
+        { name = 'vsnip' }, -- For vsnip users.
+    },
+    {
+        { name = 'buffer' },
+    })
+})
