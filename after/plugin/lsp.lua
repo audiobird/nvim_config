19f27bc3e1fe45681fee58e671a935a787640c23
audiobird <cantrellhepner@gmail.com>
2024-01-20 10:47:48 -0800

    move cmp stuff to its own file

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -5,6 +5,9 @@ lsp_zero.on_attach(function(client, bufnr)
       vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', {buffer = bufnr})
 end)
 
+-- Set up lspconfig.
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
 require('mason').setup({})
 require('mason-lspconfig').setup({
     ensure_installed = { 'clangd'},
@@ -12,6 +15,7 @@ require('mason-lspconfig').setup({
         lsp_zero.default_setup,
         clangd = function()
             require('lspconfig').clangd.setup({
+                capabilities = capabilities,
                 cmd = {
                     "clangd",
                     "--background-index",
@@ -32,29 +36,6 @@ require('mason-lspconfig').setup({
             })
         end
     },
-})
-
-local cmp = require('cmp')
-local cmp_action = require('lsp-zero').cmp_action()
-
-cmp.setup({
-    mapping = cmp.mapping.preset.insert({
-    -- `Enter` key to confirm completion
-    ['<CR>'] = cmp.mapping.confirm({select = true}),
-
-    -- Ctrl+Space to trigger completion menu
-    ['<C-Space>'] = cmp.mapping.complete(),
-
-    -- Navigate between snippet placeholder
-    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-
-    -- Scroll up and down in the completion documentation
-    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-d>'] = cmp.mapping.scroll_docs(4),
-    ['<C-j>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-    ['<C-k>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select })
-  })
 })
 
 lsp_zero.format_on_save({
