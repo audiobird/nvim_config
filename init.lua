b55442461d828ab26d95c6b60a0e4cbc73c283ef
audiobird <cantrellhepner@gmail.com>
2024-01-21 22:35:16 -0800

    remove comments

--- init.lua
+++ init.lua
@@ -2,6 +2,3 @@ require("audiobird")
 
 -- stop auto comment when new line
 vim.cmd([[autocmd FileType * set formatoptions-=ro]])
-
--- vim.o.background = "dark" -- or "light" for light mode
--- vim.cmd([[colorscheme gruvbox]])
