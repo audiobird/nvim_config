6afa9bf109f182425bfee264b1599954ab7a0810
audiobird <cantrellhepner@gmail.com>
2024-02-01 08:44:12 -0800

    add hotkey for lazygit

--- after/plugin/floaterm.lua
+++ after/plugin/floaterm.lua
@@ -1,3 +1,5 @@
+vim.keymap.set("n", "<F7>", "<cmd>FloatermNew --height=1.0 --width=1.0 --disposable lazygit<CR>")
+
 vim.keymap.set("n", "<F12>", "<cmd>FloatermToggle<CR>")
 vim.api.nvim_set_keymap('t', '<F12>', '<C-\\><C-n>:FloatermToggle<CR>', {noremap = true, silent = true})
 
