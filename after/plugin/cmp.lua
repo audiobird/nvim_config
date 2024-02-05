7fc35b852f8f64c33483c6fafb8a96e8ed456d69
audiobird <cantrellhepner@gmail.com>
2024-02-05 10:11:31 -0800

    abort cmp menu

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -1,6 +1,6 @@
 local cmp = require('cmp')
 local cmp_action = require('lsp-zero').cmp_action()
---test
+
 cmp.setup({
     completion = {
         keyword_length = 2,
@@ -18,6 +18,7 @@ cmp.setup({
     mapping = cmp.mapping.preset.insert({
         -- `Enter` key to confirm completion
         ['<CR>'] = cmp.mapping.confirm({select = true}),
+        ['<C-e>'] = cmp.mapping.abort(),
 
         -- Ctrl+Space to trigger completion menu
         ['<C-Space>'] = cmp.mapping.complete(),
