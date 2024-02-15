3b94f4512a5155f3a3087e90313572ee6d0322c8
audiobird <cantrellhepner@gmail.com>
2024-02-15 08:53:46 -0800

    vscode style cmp menu

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -1,6 +1,34 @@
 local cmp = require('cmp')
 local cmp_action = require('lsp-zero').cmp_action()
 
+local cmp_kinds = {
+    Text = '  ',
+    Method = '  ',
+    Function = '  ',
+    Constructor = '  ',
+    Field = '  ',
+    Variable = '  ',
+    Class = '  ',
+    Interface = '  ',
+    Module = '  ',
+    Property = '  ',
+    Unit = '  ',
+    Value = '  ',
+    Enum = '  ',
+    Keyword = '  ',
+    Snippet = '  ',
+    Color = '  ',
+    File = '  ',
+    Reference = '  ',
+    Folder = '  ',
+    EnumMember = '  ',
+    Constant = '  ',
+    Struct = '  ',
+    Event = '  ',
+    Operator = '  ',
+    TypeParameter = '  ',
+}
+
 cmp.setup({
     completion = {
         keyword_length = 2,
@@ -40,5 +68,12 @@ cmp.setup({
     },
     {
         { name = 'buffer' },
-    })
+    }),
+    formatting = {
+        fields = { "kind", "abbr" },
+        format = function(_, vim_item)
+            vim_item.kind = cmp_kinds[vim_item.kind] or ""
+            return vim_item
+        end,
+    },
 })
