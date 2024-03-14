010a649d74cbf975002590947f9926199cb5ff06
audiobird <cantrellhepner@gmail.com>
2024-03-13 23:15:12 -0700

    fix lots of annoying things

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,8 +1,18 @@
-local lsp_zero = require('lsp-zero')
+local lsp = require('lsp-zero')
 
-lsp_zero.on_attach(function(client, bufnr)
-    lsp_zero.default_keymaps({buffer = bufnr})
-      vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', {buffer = bufnr})
+lsp.set_sign_icons({
+    error = '✘',
+    warn = '▲',
+    hint = '⚑',
+    info = '»'
+})
+
+lsp.preset("recommended")
+
+
+lsp.on_attach(function(client, bufnr)
+    lsp.default_keymaps({ buffer = bufnr })
+    vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', { buffer = bufnr })
 end)
 
 -- Set up lspconfig.
@@ -13,9 +23,13 @@ capabilities.textDocument.completion.completionItem.snippetSupport = false
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-    ensure_installed = { 'clangd'},
+    ensure_installed = {
+        'clangd',
+        'gopls',
+        'html',
+    },
     handlers = {
-        lsp_zero.default_setup,
+        lsp.default_setup,
         clangd = function()
             require('lspconfig').clangd.setup({
                 capabilities = capabilities,
@@ -38,6 +52,27 @@ require('mason-lspconfig').setup({
                 filetypes = { "c", "c++", "cpp" },
             })
         end,
+        -- undefined vim error
+        lua_ls = function()
+            require('lspconfig').lua_ls.setup({
+
+                settings = {
+                    Lua = {
+                        diagnostics = {
+                            globals = { 'vim' }
+                        }
+                    }
+                }
+            })
+        end,
+        gopls = function()
+            require('lspconfig').gopls.setup({
+                filetypes = { "go", "gomod", "gowork", "gotmpl" },
+                settings = {
+                    -- meh, doesn't seem to work templateExtensions = { "gohtml", "tmpl", "gotmpl", "tpl", "html" },
+                },
+            })
+        end,
         tsserver = function()
             require('lspconfig').tsserver.setup({
                 capabilities = capabilities,
@@ -47,7 +82,7 @@ require('mason-lspconfig').setup({
         html = function()
             require('lspconfig').html.setup({
                 init_options = {
-                    configurationSection = { "html", "css", "javascript" },
+                    configurationSection = { "css", "javascript" },
                     embeddedLanguages = {
                         css = true,
                         javascript = true,
@@ -59,7 +94,7 @@ require('mason-lspconfig').setup({
     },
 })
 
-lsp_zero.format_on_save({
+lsp.format_on_save({
     format_opts = {
         async = false,
         timeout_ms = 10000,
@@ -67,28 +102,31 @@ lsp_zero.format_on_save({
     servers = {
         ['clangd'] = { 'cpp' },
         ['gopls'] = { 'go' },
+        ['html'] = { 'html' },
+        ['lua_ls'] = { 'lua' },
     }
 })
 
-local function setup_lsp_diags()
-    vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-    vim.lsp.diagnostic.on_publish_diagnostics,
-    {
-        virtual_text = false,
-        signs = true,
-        update_in_insert = false,
-        underline = true,
-    }
-    )
-end
-
-setup_lsp_diags()
-
-vim.o.updatetime = 150
-vim.api.nvim_create_autocmd({ "CursorHold" }, {
-    group = vim.api.nvim_create_augroup("float_diagnostic", { clear = true }),
-    callback = function ()
-        vim.diagnostic.open_float(nil, {focus=false})
-    end
-})
-
+-- auto diagnostic window. dont really like it anymore
+--local function setup_lsp_diags()
+--    vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+--        vim.lsp.diagnostic.on_publish_diagnostics,
+--        {
+--            virtual_text = false,
+--            signs = true,
+--            update_in_insert = false,
+--            underline = true,
+--        }
+--    )
+--end
+--
+--setup_lsp_diags()
+--
+--vim.o.updatetime = 150
+--vim.api.nvim_create_autocmd({ "CursorHold" }, {
+--    group = vim.api.nvim_create_augroup("float_diagnostic", { clear = true }),
+--    callback = function()
+--        vim.diagnostic.open_float(nil,
+--            { focusable = false, scope = "line", close_events = { "CursorMoved", "CursorMovedI", "BufHidden", "InsertCharPre", "WinLeave" }, focus = false })
+--    end
+--})
